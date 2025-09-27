2c95e556e8c7e65024c2ff4b7f0c6f7ac3e07e7f
Ndolo Mercy <mercymwendend444@gmail.com>
2025-09-27 12:42:54 +0300

    Completed Lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self,name,breed="Mutt"):
+        self.name = name
+        self.breed= breed
+    
+    def bark(self):
+        print("Woof!")
+
+    def showing_self(self):
+        return self
+    
+    def adopt(self,owner_name):
+        self.owner = owner_name
+
+fido = Dog("Fido","ducky")
+# print(fido is fido.showing_self())
+print(fido.breed)
+# fido.owner = "Sophie"
+# print(fido.owner)
+fido.adopt("Sophie")
+print(fido.owner)
+# print(fido.favourite_toy)
